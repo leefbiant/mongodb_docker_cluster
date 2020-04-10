765cc0c7fe46d91a436b0d73a2843a973c0e1867
root <bt731001@163.com>
2020-04-10 14:25:05 +0800

    add init

---
+++ init.sh
@@ -0,0 +1,16 @@
+mkdir -p mongos
+mkdir -p mongos/etc
+
+mkdir -p shard_server01
+mkdir -p shard_server01/etc
+
+mkdir -p shard_server02
+mkdir -p shard_server02/etc
+
+mkdir -p rs_config_server
+mkdir -p rs_config_server/etc
+
+mkdir -p mongo-files
+#openssl rand -base64 741 > mongo-files/key.file
+#chmod 600 mongo-files/key.file
+#chown 999 mongo-files/key.file
